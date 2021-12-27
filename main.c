03d8011285d9799c36f9d2cae65ee85f86a8affe
ayoub ben hamou <ayoubbenhamou0731@gmail.com>
2021-12-28 00:53:38 +0100

    init

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ayoub <ayoub@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/27 23:59:41 by ayoub             #+#    #+#             */
-/*   Updated: 2021/12/28 00:29:53 by ayoub            ###   ########.fr       */
+/*   Updated: 2021/12/28 00:50:53 by ayoub            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ int	main(int ac, char **av)
 	av++;
 	init_stack(&a, ac - 1, av);
 	init_stack(&b, ac - 1, NULL);
+	print_stack(a);
+	print_stack(b);
+	
+	//ft_pb(&b, &a);
+	//ft_pb(&b, &a);
+	//ft_sa(&a);
+	ft_rra(&a);
+	
 	print_stack(a);
 	print_stack(b);
 	return (0);
